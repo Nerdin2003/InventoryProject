1e13d0995bfc5897aef9d1cb49753aa078589b8a
Nerdin2003 <73284307+Nerdin2003@users.noreply.github.com>
2025-10-17 17:48:02 +0300

    finished addItem()

--- InventoryFrontend/index.js
+++ InventoryFrontend/index.js
@@ -10,7 +10,7 @@ async function fetchItems(){
 
     items.forEach(item => {
         const li = document.createElement('li');
-        li.textContent = `${item.id} - Quantity: ${item.quantity}  - Price: $${item.price.toFixed(2)}`
+        li.textContent = `${item.name} - Quantity: ${item.quantity}  - Price: $${item.price.toFixed(2)}`
         itemList.appendChild(li);
         
         const deleteButton = document.createElement('button');
@@ -19,10 +19,26 @@ async function fetchItems(){
         li.appendChild(deleteButton);
     });
 
+} 
+
+async function addItem() {
+    const name = document.getElementById("item-name").value;
+    const quantity = document.getElementById("item-quantity").value;
+    const price = document.getElementById("item-price").value;
+
+    const item = {name, quantity: parseInt(quantity), price: parseFloat(price)};
+
+    await fetch(apiUrl, {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify(item)
+    });
+
+    fetchItems();
 }
 
 async function deleteItem(id) {
     //implement delete func
 }
 
-window.onload = fetchItems(); 
+window.onload = fetchItems; 
